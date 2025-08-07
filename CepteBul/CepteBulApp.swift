8e6c60b6239db4ef758ef3ea56e5ab7906531ee0
Hamza Tüfekçi <hamzatufekci1234@gmail.com>
2025-08-07 21:18:29 +0300

    Initial Commit

---
+++ CepteBul/CepteBulApp.swift
@@ -0,0 +1,17 @@
+//
+//  CepteBulApp.swift
+//  CepteBul
+//
+//  Created by Hamza Tüfekçi on 7.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CepteBulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CepteBulTests/CepteBulTests.swift
@@ -0,0 +1,17 @@
+//
+//  CepteBulTests.swift
+//  CepteBulTests
+//
+//  Created by Hamza Tüfekçi on 7.08.2025.
+//
+
+import Testing
+@testable import CepteBul
+
+struct CepteBulTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
