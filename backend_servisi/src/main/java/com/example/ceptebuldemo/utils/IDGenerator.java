487463ecc6d108dbab3729b782d7beb2040da584
HamzaTufekcii <123481115+HamzaTufekcii@users.noreply.github.com>
2025-08-09 22:04:08 +0300

    Delete backend_servisi directory

--- backend_servisi/src/main/java/com/example/ceptebuldemo/utils/IDGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.example.ceptebuldemo.utils;
-
-import java.util.UUID;
-
-public class IDGenerator {
-    public static String generateUserId() {
-        return UUID.randomUUID().toString();
-    }
-}
--- backend_servisi/src/main/java/com/example/ceptebuldemo/utils/TokenUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.ceptebuldemo.utils;
-
-import java.util.UUID;
-
-public class TokenUtil {
-    public static String generateToken() {
-        return UUID.randomUUID().toString();
-    }
-}
